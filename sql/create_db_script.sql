99348fbb1ae65aba10d7feb9ef9981931e7f3362
Alexandru Covaliuc <covaliuc.a@yahoo.com>
2022-12-19 14:11:54 +0100

    adaugat baza de date + script

--- sql/create_db_script.sql
+++ sql/create_db_script.sql
@@ -8,4 +8,8 @@ CREATE TABLE quiz (
 	answer_4 varchar(127),
 	correct_answer varchar(3));
 
-INSERT INTO quiz VALUES (1,'xx?','a','b','c?',">>>>?d?<<<<",'A2');
+INSERT INTO quiz VALUES (1,'In ce an s-a nascut Alexandra Irina Maruta, cunoscuta drept Andra?','1984','2001','1986',"1896",'A3');
+INSERT INTO quiz VALUES (2,'Vocea Andrei nu se aude doar la radio! Alege filmul in care Andra dubleaza in limba romana un personaj!','Strumfii (2011)','In căutarea lui Nemo (2003)','Ted (2012) ',"The Room (2003)",'A1');
+INSERT INTO quiz VALUES (3,'Care este numele sotului Andrei?','Catalin Maruta','Emil Constantinescu','Nedelcu Andrew Eduard',"Stefan Avram Cherescu",'A1');
+INSERT INTO quiz VALUES (4,'In ce an a concertat Andra pe Stadionul "Emil Alexandrescu" din Iasi, in cadrul festivalului INIMO?','2022','2002','2021',"2020",'A1');
+INSERT INTO quiz VALUES (5,'Completati versurile: "Cafeneaua-n care ne-ntalneam / Acum o ocolesc / Mi-aminteste de..."','Primul sarut','Prima intalnire','Intaiul sarut','Intaiul "te iubesc"','A4');
